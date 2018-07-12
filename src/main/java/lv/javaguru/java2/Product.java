121c8bf12f40cf548a07d23d864476eb2d2662bc
andrejs.sobolevs <viperjuga13@inbox.lv>
2018-07-11 18:10:05 -0700

    Initial commit

---
+++ src/main/java/lv/javaguru/java2/Product.java
@@ -0,0 +1,33 @@
+package lv.javaguru.java2;
+
+public class Product {
+
+    private String title;
+    private String description;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "title=" + title +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+}
---
+++ src/test/java/lv/javaguru/java2/Test.java
@@ -0,0 +1,5 @@
+package lv.javaguru.java2;
+
+public class Test {
+
+}

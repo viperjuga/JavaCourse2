4ca522bf708257729960c43c3265802483bb27f1
Angelina Soboleva <40835328+AngelinaSaer@users.noreply.github.com>
2018-08-01 19:18:22 +0300

    Change database interface

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,17 +1,42 @@
 package database;
 
-import domainProduct.Product;
+import domain.Car;
+import domain.Rent;
+import domain.User;
+import enums.CarStatus;
+import enums.UserType;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface Database {
 
-    void addProduct(Product product);
+    //user
+    void addUser(User user);
+    void updateUser (User user);
+    Optional<User> getUserById (Long id);
+    List<User> getAllUsersByType(UserType type);
 
-    Optional<Product> getByTitle(String title);
 
-    boolean remove(Product product);
+    //cars
+    void addCar(Car car);
+    void removeCar(Car car);
+    void updateCar(Car car);
+    List<Car> getAllCars ();
+    Optional<Car> getCarById(Long id);
+    List<Car> getFreeCarsForDate(Date date);
+    //TODO add request with filters
 
-    List<Product> getAllProducts();
+
+    //rent
+    void addRent (Rent rent);
+    void updateRentStatus (long rentId, CarStatus status);
+    List<Rent> getAllRents ();
+    List<Rent> getRentsForCar(Long carId);
+    List<Rent> getCurrentClientRents(Long clientId);
+    List<Rent> getCurrentOperatorRents (Long operatorId);
+    List<Rent> getRentWithStatus(CarStatus status);
+
+
 }

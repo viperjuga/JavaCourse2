45f9b4a743d882df580fbd9ead705791638d7ace
Angelina Soboleva <40835328+AngelinaSaer@users.noreply.github.com>
2018-08-01 19:37:31 +0300

    Finish with Services

--- src/main/java/services/rent/AddRentService.java
+++ src/main/java/services/rent/AddRentService.java
@@ -1,6 +1,17 @@
 package services.rent;
 
+import database.Database;
+import domain.Rent;
+
 public class AddRentService {
 
+    private Database database;
 
+    public AddRentService (Database database){
+        this.database = database;
+    }
+
+    public void addRent (Rent rent){
+        database.addRent(rent);
+    }
 }
--- src/main/java/services/rent/UpdateRentService.java
+++ src/main/java/services/rent/UpdateRentService.java
@@ -1,4 +1,16 @@
 package services.rent;
 
+import database.Database;
+import enums.CarStatus;
+
 public class UpdateRentService {
+    private Database database;
+
+    public UpdateRentService (Database database){
+        this.database = database;
+    }
+
+    public void updateRentStatus (Long rentId, CarStatus status){
+        database.updateRentStatus(rentId, status);
+    }
 }
